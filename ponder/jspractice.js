36662787c96c5e7b5e555ee916d0ca4b66efd2c4
zeekster3 <zeeekster3@gmail.com>
2025-05-03 21:47:00 -0600

    modified:   ponder/jspractice.html 	modified:   ponder/jspractice.js modified:   mission/index.html 	modified:   mission/styles.css

--- ponder/jspractice.js
+++ ponder/jspractice.js
@@ -1,3 +1,5 @@
+// variables vs constants
+
 const PI = 3.14;
 let radius = 3;
 
@@ -10,3 +12,32 @@ radius = 20;
 area = radius * radius * PI;
 
 console.log(area);
+
+
+// type coersion
+const one = 1;
+const two = '2';
+
+let result = one * two;
+
+console.log(result);
+
+result = one + two;
+
+console.log(result);
+
+//scope
+let global = "Isaac says,\"I'm global\" ";
+
+function exampleFunction(){
+let block = "I am block level or local"
+console.log(block);
+console.log(global);
+};
+
+console.log(block);
+console.log(global);
+exampleFunction();
+
+
+global = "I am also global";
